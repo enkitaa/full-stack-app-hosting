d3ac6034d5f7e6f42a4d1c4dcb8f1a1ca3836fcc
enkitaa <ankkita95@gmail.com>
2021-12-02 15:34:53 +0530

    fix: remove build errors

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:5432/api/v0'
+  apiHost: 'http://ugapi.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
